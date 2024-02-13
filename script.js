a605527f392f01c567215eb6cf9e00013a40bb81
GilmaraSantos <gilmara.dosantos@pearson.com>
2024-02-13 20:11:08 -0300

    ajuste

---
+++ script.js
@@ -0,0 +1,10 @@
+function changeColor() {
+    var input = document.getElementById("myInput");
+    input.classList.add("clicked");
+
+}
+function changeColor() {
+    var button = document.querySelector("button");
+    button.style.backgroundColor = "blue";
+    button.style.color = "white";
+}
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  ccontent: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["DM Sans","sans-serif"]
+      }
+    },
+  },
+  plugins: [],
+}
+
